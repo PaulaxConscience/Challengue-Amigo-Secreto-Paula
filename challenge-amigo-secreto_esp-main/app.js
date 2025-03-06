8db26b1ab326cfccf031baed838a7df9ff211597
Paula Nuñez <paula.csed@gmail.com>
2025-03-06 17:00:20 -0300

    Creando lista con el boton amigos

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let lista = [];
+function agregarAmigo(){
+    let nombre = document.getElementById("amigo");
+    let nombre2 = nombre.value.trim();
+    lista.push(nombre2);
+    console.log(lista);
+    return;
+
+}
