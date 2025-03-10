660576f86591ef6e24cd39e03ea8f54787628a03
Paula Nuñez <paula.csed@gmail.com>
2025-03-10 16:39:15 -0300

    Ya Finazalizado

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,10 +1,42 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let lista = [];
 function agregarAmigo(){
-    let nombre = document.getElementById("amigo");
-    let nombre2 = nombre.value.trim();
-    lista.push(nombre2);
-    console.log(lista);
-    return;
-
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+    if (nombre === "") {
+    alert ("Por favor, inserte un nombre");
+    } else {
+        lista.push(nombre);
+        input.value= "";
+        if (lista.length>0){
+            console.log("La lista tiene amigos:", lista)
+        }
+        mostrarLista();
+    }
 }
+function mostrarLista() {
+    // Obtener el elemento donde se mostrará la lista
+    let listaHTML = document.getElementById("listaAmigos");
+
+    // Limpiar la lista en el HTML antes de actualizarla
+    listaHTML.innerHTML = "";
+
+    // Agregar cada nombre como un nuevo elemento de lista (<li>)
+    lista.forEach(function(nombre) {
+        let li = document.createElement("li");
+        li.textContent = nombre;
+        listaHTML.appendChild(li);
+    });
+}
+function sortearAmigo(){
+    let indiceAmigoSorteado = Math.floor(Math.random()*lista.length);
+    let amigoSorteado = lista[indiceAmigoSorteado];
+    console.log(amigoSorteado);
+    mostrarAmigoSorteado(amigoSorteado);
+}
+function mostrarAmigoSorteado(amigoSorteado){
+    let listaHTML = document.getElementById("resultado");
+    listaHTML.textContent = "El amigo secreto sorteado es: " + amigoSorteado;
+}
+
+
